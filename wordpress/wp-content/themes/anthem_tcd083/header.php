51cc175156007ea6697f4b215a705fcdcbbf79c7
Toshiya Shimizu <shimizu@hakarai.co.jp>
2022-12-20 11:58:36 +0900

    fix: install Google Tag Manager

--- wordpress/wp-content/themes/anthem_tcd083/header.php
+++ wordpress/wp-content/themes/anthem_tcd083/header.php
@@ -6,7 +6,14 @@
 <?php } else { ?>
 <head>
 <?php }; ?>
-<meta charset="<?php bloginfo('charset'); ?>">
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                              })(window,document,'script','dataLayer','GTM-578MDFD');</script>
+  <!-- End Google Tag Manager -->
+  <meta charset="<?php bloginfo('charset'); ?>">
 <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
 <meta name="viewport" content="width=device-width">
 <title><?php wp_title('|', true, 'right'); ?></title>
@@ -23,7 +30,10 @@
 <?php wp_head(); ?>
 </head>
 <body id="body" <?php body_class(); ?>>
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-578MDFD"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 <?php
      if ($options['show_load_screen'] == 'type2') {
        if(is_front_page()){
